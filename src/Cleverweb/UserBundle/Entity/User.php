3821b0ff3cd33bf5de4ce3664aeb82f71fb18305
Vadim Madumarov <me@madumarov.me>
2014-04-04 16:55:16 +0400

    Created UserBundle.

---
+++ src/Cleverweb/UserBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 4/4/14
+ * Time: 2:47 PM
+ */
+
+namespace Cleverweb\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+} 
