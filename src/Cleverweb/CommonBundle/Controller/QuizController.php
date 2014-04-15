964cc29d6141c3078113132fb0050b36ad0aa30a
Vadim Madumarov <me@madumarov.me>
2014-04-15 12:45:47 +0400

    Created button "add quiz" and code for creating quiz.

--- src/Cleverweb/CommonBundle/Controller/QuizController.php
+++ src/Cleverweb/CommonBundle/Controller/QuizController.php
@@ -6,6 +6,7 @@ use Cleverweb\CommonBundle\Entity\Quiz;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class QuizController extends Controller
 {
@@ -13,15 +14,25 @@ class QuizController extends Controller
      * @Route("/quizzes/new", name="cleverweb_common_quiz_create")
      * @Template()
      */
-    public function createQuizAction()
+    public function createQuizAction(Request $request)
     {
         $quiz = new Quiz();
+        $em = $this->getDoctrine()->getManager();
 
         $form = $this->createFormBuilder($quiz)
             ->add('name', 'text')
             ->add('category')
+            ->add('add quiz', 'submit')
             ->getForm();
 
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            $em->persist($quiz);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('cleverweb_common_quiz_create'));
+        }
 
         return array(
             'form' => $form->createView()
